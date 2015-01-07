cb038004bb7e3d352b75623ac3935081948f6b9c
Yasar icli <hello@yasaricli.com>
2015-01-07 09:50:17 +0200

    package updated

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'libreboard:slugify',
+  name: 'yasaricli:slugify',
   summary: 'Remove special characters from a string',
   version: '0.0.7',
-  git: 'https://github.com/libreboard/meteor-slugify.git'
+  git: 'https://github.com/yasaricli/meteor-slugify.git'
 });
 
 Package.onUse(function(api, where) {
@@ -22,6 +22,6 @@ Package.onTest(function(api, where) {
   api.versionsFrom('1.0.2.1');
   api.use('underscore', where);
   api.use('tinytest', where);
-  api.use('libreboard::slugify', where);
+  api.use('yasaricli::slugify', where);
   api.addFiles('tests.js', where);
 });
