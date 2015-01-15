42810cb1a08af293d4eec796c545c4f958990ad4
Yasar içli <hello@yasaricli.com>
2015-01-15 06:34:22 -0500

    package version reset.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'yasaricli:slugify',
   summary: 'Remove special characters from a string',
-  version: '0.0.6',
+  version: '0.0.1',
   git: 'https://github.com/yasaricli/meteor-slugify.git'
 });
 
