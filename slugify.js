01920c15448fb3421d22c762742d5d418c7a9e65
Yasar icli <hello@yasaricli.com>
2015-07-07 11:02:06 +0300

    quotes and double quotes removed regex. #1

--- slugify.js
+++ slugify.js
@@ -3,24 +3,27 @@ var defaultReplacement = function(replacement) {
 };
 
 slugify = function(string, replacement) {
-  var replacement = defaultReplacement(replacement);
+  var replacement = defaultReplacement(replacement),
+      out = '';
+
   if (!_.isString(string)) {
     return '';
   }
 
-  var result = '';
-  for (var i=0; i < string.length; i++) {
-    var ch = string[i];
-    if (CHAR_MAP[ch]) {
+  _.forEach(string.split(''), function(ch) {
+
+    if (_.has(CHAR_MAP, ch)) {
       ch = CHAR_MAP[ch];
     }
-    ch = ch.replace(/[^\w\s$\*\_\+~\.\(\)\'\"\-]/g, ''); // allowed
-    result += ch;
-  }
-  result = result.replace(/^\s+|\s+$/g, ''); // trim leading/trailing spaces
-  result = result.replace(/[-\s]+/g, replacement); // convert spaces
-  result.replace("#{replacement}$", ''); // remove trailing separator
-  return result.toLowerCase();
+
+    ch = ch.replace(/[^\w\s$\*\_\+~\.\(\)\-]/g, ''); // allowed
+    out += ch;
+  });
+
+  out = out.replace(/^\s+|\s+$/g, ''); // trim leading/trailing spaces
+  out = out.replace(/[-\s]+/g, replacement); // convert spaces
+  out.replace("#{replacement}$", ''); // remove trailing separator
+  return out.toLowerCase();
 };
 
 if (Meteor.isClient) {
